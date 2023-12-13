d45dc2a3393c30b7ac91908ea3eeb6f73c85f872
EcoGitLab <gitlab@ecoservice.de>
2023-12-13 13:53:50 +0000

    Update ecoservice_financeinterface_datev_import

---
+++ eco/ecoservice_financeinterface_datev_import/CHANGELOG.rst
@@ -0,0 +1,22 @@
+Changelog
+=========
+
+16.0.1.0.1
+----------
+* fix change log layout.
+
+16.0.1.0.0
+----------
+* Migration to 16.0.
+
+15.0.1.1.3
+----------
+* delete Start- and Ende Date from Datev import.
+
+15.0.1.1.2
+----------
+* fix Automatic Konto by datev import.
+
+15.0.1.0.0
+----------
+* Migration to 15.0
---
+++ eco/ecoservice_financeinterface_datev_import/__manifest__.py
@@ -0,0 +1,29 @@
+# Part of Odoo. Developed by ecoservice (Uwe Böttcher und Falk Neubert GbR).
+# See COPYRIGHT and LICENSE at the root directory of this module for full copyright and licensing details.
+
+{
+    'name': 'Financial Interface DATEV Import',
+    'summary': 'This module allows you to import accounting entries.',
+    'version': '16.0.1.0.1',
+    'author': 'ecoservice',
+    'website': 'https://ecoservice.de/shop/product/odoo-datev-export-53',
+    'live_test_url': 'https://eco-finance-interface-13-0.test.ecoservice.de/',
+    'support': 'financeinterface@ecoservice.de',
+    'license': 'OPL-1',
+    'category': 'Accounting',
+    'depends': [
+        'base',
+        'account',
+        'ecoservice_financeinterface',
+        'ecoservice_financeinterface_datev',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/import_datev_sequence.xml',
+        'views/import_datev.xml',
+        'views/import_datev_menu.xml',
+    ],
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+}
