e58f6a21543f57298b5d7a7feb2e7efb091076cb
EcoGitLab <gitlab@ecoservice.de>
2024-01-24 14:12:19 +0000

    Update ecoservice_german_documents_stock

--- eco/ecoservice_german_documents_stock/models/stock_picking.py
+++ eco/ecoservice_german_documents_stock/models/stock_picking.py
@@ -8,6 +8,9 @@ class StockPicking(models.Model):
     _name = 'stock.picking'
     _inherit = ['stock.picking', 'eco_report.mixin']
 
+    signing_date = fields.Date(string='Signing Date', store=True, readonly=True, copy=False)
+    signer_name = fields.Char(string='Signer Name', store=True, readonly=True, copy=False)
+
     # region Compute Methods
     def _compute_date(self):
         for rec in self:
@@ -42,3 +45,16 @@ class StockPicking(models.Model):
             '.report_stock_picking_operation_template_without_logo',
         ]
     # endregion
+
+    def _attach_sign(self):
+        self.ensure_one()
+
+        current_date = fields.Date.today()
+        signer_name = self.partner_id.name if self.partner_id else None
+
+        if signer_name:
+            self.write({'signing_date': current_date, 'signer_name': signer_name})
+        else:
+            self.write({'signing_date': current_date})
+
+        return super()._attach_sign()
