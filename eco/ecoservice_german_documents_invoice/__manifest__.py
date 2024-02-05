ebde7a6b38f6af95056462cb5f704963b2675462
EcoGitLab <gitlab@ecoservice.de>
2024-02-05 16:19:14 +0000

    Update ecoservice_german_documents_invoice

--- eco/ecoservice_german_documents_invoice/__manifest__.py
+++ eco/ecoservice_german_documents_invoice/__manifest__.py
@@ -5,7 +5,7 @@
     'name': 'German Documents (Invoice)',
     'summary': 'Designed German Documents for Odoo.',
     'category': 'Base',
-    'version': '16.0.1.1.3',
+    'version': '16.0.1.1.4',
     'license': 'OPL-1',
     'application': False,
     'installable': True,
