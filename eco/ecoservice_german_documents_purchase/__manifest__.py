ac1b891aa87eef01977e7bcc62b43b5e02e2ea3f
EcoGitLab <gitlab@ecoservice.de>
2024-02-08 10:57:20 +0000

    Update ecoservice_german_documents_purchase

--- eco/ecoservice_german_documents_purchase/__manifest__.py
+++ eco/ecoservice_german_documents_purchase/__manifest__.py
@@ -5,7 +5,7 @@
     'name': 'German Documents (Purchase)',
     'summary': 'Designed German Documents for Odoo.',
     'category': 'Base',
-    'version': '16.0.1.0.3',
+    'version': '16.0.1.0.5',
     'license': 'OPL-1',
     'application': False,
     'installable': True,
