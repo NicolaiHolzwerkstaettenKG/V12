142622cab0d4e965f3bc167e08fb3e0ccdf0f3ea
EcoGitLab <gitlab@ecoservice.de>
2024-07-11 11:49:31 +0000

    Update ecoservice_german_documents_repair_order

---
+++ eco/ecoservice_german_documents_repair_order/__manifest__.py
@@ -0,0 +1,44 @@
+# Developed by ecoservice (Uwe Böttcher und Falk Neubert GbR).
+# See COPYRIGHT and LICENSE files in the root directory of this module for full details.
+{
+    # App Information
+    'name': 'German Documents (Repair Order)',
+    'summary': 'Designed German Documents for Odoo.',
+    'version': '16.0.1.0.0',
+    'category': 'Base',
+    'license': 'OPL-1',
+    # Author
+    'author': 'ecoservice GbR',
+    'maintainer': 'ecoservice GbR',
+    'website': 'https://www.ecoservice.de',
+    'support': 'info@ecoservice.de',
+    # Odoo Apps Store
+    'images': [
+        'images/main_screenshot.png',
+    ],
+    # 'live_test_url': 'https://...',
+    # App Installation
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+    # Dependencies
+    'depends': [
+        'repair',
+        # ecoervice
+        'ecoservice_german_documents_base',
+    ],
+    'external_dependencies': {},
+    # Data
+    'data': [
+        'reports/repair_templates_order_css.xml',
+        'reports/repair_templates_repair_order.xml',
+    ],
+    # Demo files - only installed and updated in demo Mode
+    'demo': [],
+    # Asset Bundles
+    'assets': {},
+    # Hooks
+    'pre_init_hook': '',
+    'post_init_hook': '',
+    'uninstall_init_hook': '',
+}
