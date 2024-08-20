95300265c46ed8677b2b4354c5858c0ba7bd8355
EcoGitLab <gitlab@ecoservice.de>
2024-08-20 12:26:34 +0000

    Update ecoservice_german_documents_base

--- eco/ecoservice_german_documents_base/__manifest__.py
+++ eco/ecoservice_german_documents_base/__manifest__.py
@@ -5,7 +5,7 @@
     'name': 'German Documents (Base)',
     'summary': 'Designed German Documents for Odoo.',
     'category': 'Base',
-    'version': '16.0.1.2.8',
+    'version': '16.0.1.2.9',
     'license': 'OPL-1',
     'application': False,
     'installable': True,
