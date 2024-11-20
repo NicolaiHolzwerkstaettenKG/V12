5cc6b78cd4449f7b601fe6ddfbc644943dc52b93
EcoGitLab <gitlab@ecoservice.de>
2024-11-20 11:22:15 +0000

    Update ecoservice_financeinterface

--- eco/ecoservice_financeinterface/models/ecofi/ecofi.py
+++ eco/ecoservice_financeinterface/models/ecofi/ecofi.py
@@ -115,20 +115,11 @@ class Ecofi(models.Model):
         return ecofi_csv, log
 
     def ecofi_posted_moves(self, journal_ids):
-        sale_journals = []
-        journals_rest = []
-
-        for journal in journal_ids:
-            if journal.type == 'sale':
-                sale_journals.append(journal.id)
-            else:
-                journals_rest.append(journal.id)
-
         move_ids = self.env['account.move'].sudo()
 
         # 110111: Get invoices based on the invoice date only.
         move_ids += move_ids.search([
-            ('journal_id', 'in', sale_journals),
+            ('journal_id', 'in', journal_ids.ids),
             ('state', '=', 'posted'),
             ('vorlauf_id', '=', False),
             ('invoice_date', '>=', self.date_from),
@@ -145,7 +136,7 @@ class Ecofi(models.Model):
 
         # 110111: Get non-invoices based on the accounting date only.
         move_ids += move_ids.search([
-            ('journal_id', 'in', journals_rest),
+            ('journal_id', 'in', journal_ids.ids),
             ('state', '=', 'posted'),
             ('vorlauf_id', '=', False),
             ('date', '>=', self.date_from),
