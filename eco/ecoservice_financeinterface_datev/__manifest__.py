d03ab8269b7f01bf6a0b7a772e511f1695deb363
EcoGitLab <gitlab@ecoservice.de>
2024-12-17 13:03:10 +0000

    Update ecoservice_financeinterface_datev

--- eco/ecoservice_financeinterface_datev/__manifest__.py
+++ eco/ecoservice_financeinterface_datev/__manifest__.py
@@ -10,10 +10,12 @@
     'application': True,
     'installable': True,
     # Author
-    'author': 'ecoservice',
+    'author': 'ecoservice GbR',
     'website': 'https://ecoservice.de/shop/product/odoo-datev-export-53',
     # Odoo Apps Store
     #'live_test_url': 'https://eco-finance-interface-14-0.test.ecoservice.de/',
+    'price': 650.00,
+    'currency': 'EUR',
     'support': 'financeinterface@ecoservice.de',
     'images': [
         'images/main_screenshot.png',
