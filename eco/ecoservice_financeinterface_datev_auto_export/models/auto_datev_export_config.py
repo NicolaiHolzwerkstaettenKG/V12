9f0786f3984f0bc058017d12fa8759c9c05b3e75
EcoGitLab <gitlab@ecoservice.de>
2025-01-30 18:48:06 +0000

    Update ecoservice_financeinterface_datev_auto_export

--- eco/ecoservice_financeinterface_datev_auto_export/models/auto_datev_export_config.py
+++ eco/ecoservice_financeinterface_datev_auto_export/models/auto_datev_export_config.py
@@ -40,6 +40,20 @@ class AutoDatevExportConfig(models.Model):
 
     company_id = fields.Many2one('res.company', string='Company', required=True)
 
+    export_backtrack = fields.Selection(
+        selection=[
+            ('last_export', 'Last Automatic Export'),
+            ('start_of_year', 'Start of the Year'),
+            ('last_export_minus_1', 'Last Export - 1 Month'),
+            ('last_export_minus_2', 'Last Export - 2 Month'),
+            ('last_export_minus_3', 'Last Export - 3 Month'),
+            ('previous_year_start', 'Start of the Last Year'),
+        ],
+        string="Export Backtrack",
+        default='last_export',
+        required=True,
+    )
+
     # endregion
 
     # region Constraints
@@ -66,10 +80,31 @@ class AutoDatevExportConfig(models.Model):
         current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         self.env['ir.config_parameter'].sudo().set_param(param_name, current_time)
 
+    def _calculate_start_date(self, config, last_run, default_period):
+
+        if config.export_backtrack == 'start_of_year':
+            start_date = datetime(datetime.now().year, 1, 1).date()
+        elif config.export_backtrack == 'previous_year_start':
+            start_date = datetime(datetime.now().year - 1, 1, 1).date()
+        elif config.export_backtrack.startswith('last_export_minus_'):
+            months_offset = int(config.export_backtrack.split('_')[-1])
+            start_date = (last_run + relativedelta(months=-months_offset)).date() if last_run else default_period
+        elif config.export_backtrack == 'last_export':
+            start_date = last_run.date() if last_run else default_period
+        else:
+            start_date = default_period
+
+        if last_run:
+            start_date = min(last_run.date(), default_period, start_date)
+        else:
+            start_date = min(default_period, start_date)
+
+        return start_date
+
     def auto_mail_send(self):
         companies = self.env['res.company'].search([])
         for company in companies:
-            company_self = self.with_context(force_company=company.id)
+            company_self = self.with_company(company.id)
 
             configs = self.env['auto.datev.export.config'].search(
                 [('company_id', '=', company.id)]
@@ -84,53 +119,51 @@ class AutoDatevExportConfig(models.Model):
         if last_run and (datetime.now() - last_run) < timedelta(days=1):
             return  # Skip if the last run was less than 1 day ago
 
-        if last_run:
-            start_date = last_run.date()
-        else:
-            start_date = datetime.today().date()
         configs_daily = configs.filtered(lambda a: a.period == 'daily')
         if configs_daily:
-            end_date = datetime.today().date()
-            ecofi_list = self.generate_financial_report_csv(start_date, end_date)
-            if ecofi_list:
-                self.send_mail_to_partners(configs_daily, ecofi_list)
-                self._set_last_run('daily', company_id)
+            for config in configs_daily:
+                start_date = self._calculate_start_date(config, last_run, datetime.today().date())
+                end_date = datetime.today().date()
+                ecofi_list = self.generate_financial_report_csv(start_date, end_date)
+                if ecofi_list:
+                    self.send_mail_to_partners(config, ecofi_list)
+                    self._set_last_run('daily', company_id)
 
     def auto_send_mail_weekly(self, configs, company_id):
         last_run = self._get_last_run('weekly', company_id)
         if last_run and (datetime.now() - last_run) < timedelta(weeks=1):
             return  # Skip if the last run was less than 1 week ago
 
-        if last_run:
-            start_date = last_run.date()
-        else:
-            start_date = (fields.Datetime.now() - timedelta(days=7)).date()
         configs_weekly = configs.filtered(lambda a: a.period == 'weekly')
         if configs_weekly:
-            if datetime.today().strftime('%A') in ['Monday', 'Montag']:
-                end_date = datetime.today().date()
-                ecofi_list = self.generate_financial_report_csv(start_date, end_date)
-                if ecofi_list:
-                    self.send_mail_to_partners(configs_weekly, ecofi_list)
-                    self._set_last_run('weekly', company_id)
+            for config in configs_weekly:
+                start_date = self._calculate_start_date(
+                    config, last_run, (fields.Datetime.now() - timedelta(days=7)).date()
+                )
+                if datetime.today().strftime('%A') in ['Monday', 'Montag']:
+                    end_date = datetime.today().date()
+                    ecofi_list = self.generate_financial_report_csv(start_date, end_date)
+                    if ecofi_list:
+                        self.send_mail_to_partners(config, ecofi_list)
+                        self._set_last_run('weekly', company_id)
 
     def auto_send_mail_monthly(self, configs, company_id):
         last_run = self._get_last_run('monthly', company_id)
         if last_run and (datetime.now() - last_run) < timedelta(days=30):
             return  # skip if the last run was less than 1 month ago
 
-        if last_run:
-            start_date = last_run.date()
-        else:
-            start_date = (fields.Datetime.now() + relativedelta(months=-1)).date()
         configs_monthly = configs.filtered(lambda a: a.period == 'monthly')
         if configs_monthly:
-            if datetime.today().strftime('%d') == '01':
-                end_date = datetime.today().date()
-                ecofi_list = self.generate_financial_report_csv(start_date, end_date)
-                if ecofi_list:
-                    self.send_mail_to_partners(configs_monthly, ecofi_list)
-                    self._set_last_run('monthly', company_id)
+            for config in configs_monthly:
+                start_date = self._calculate_start_date(
+                    config, last_run, (fields.Datetime.now() + relativedelta(months=-1)).date()
+                )
+                if datetime.today().strftime('%d') == '01':
+                    end_date = datetime.today().date()
+                    ecofi_list = self.generate_financial_report_csv(start_date, end_date)
+                    if ecofi_list:
+                        self.send_mail_to_partners(config, ecofi_list)
+                        self._set_last_run('monthly', company_id)
 
     def generate_financial_report_csv(self, start_date, end_date):
         ecofi_list = []
@@ -145,6 +178,7 @@ class AutoDatevExportConfig(models.Model):
             ecofi = self.env['ecofi'].search([
                 ('date_from', '=', start_date),
                 ('date_to', '=', end_date),
+                ('company_id', '=', self.env.company.id),
             ])
 
             if ecofi:
@@ -177,8 +211,12 @@ class AutoDatevExportConfig(models.Model):
             template = self.env['mail.template'].browse(template_id)
             if template:
                 attachment = self.env['ir.attachment']
-                for ecofi in ecofi_list:
-                    attachment += self.create_attachment(ecofi)
+                for ecofi_item in ecofi_list:
+                    if len(ecofi_item) > 1:
+                        for ecofi in ecofi_item:
+                            attachment |= self.create_attachment(ecofi)
+                    else:
+                        attachment |= self.create_attachment(ecofi_item)
 
                     module_is_installed = self.env['ir.module.module'].search(
                         [
@@ -187,7 +225,7 @@ class AutoDatevExportConfig(models.Model):
                         ]
                     )
                     if module_is_installed:
-                        if ecofi.xml_export_file:
+                        if ecofi_item.xml_export_file:
                             xml_attachment_file = self.env['ir.attachment'].sudo().create({
                                 'name': ecofi.name + '.zip',
                                 'datas': ecofi.xml_export_file,
@@ -201,16 +239,23 @@ class AutoDatevExportConfig(models.Model):
                     config.id,
                     ['subject', 'body_html', 'email_from', 'email_to', 'partner_to', 'attachment_ids']
                 )
-                values.update({
-                    'subject': (
-                        f"Financial Summary Report From {ecofi_list[0].date_from.strftime('%d-%m-%Y')} "
-                        f"To {ecofi_list[-1].date_to.strftime('%d-%m-%Y')}"
-                    ),
-                    'email_from': self.env.user.email,
-                    'email_to': email_to,
-                    'attachment_ids': [(6, 0, attachment.ids)],
-                    'body_html': template.body_html,
-                })
+
+                if ecofi_list:
+                    date_from = min(ecofo.date_from for ecofi_item in ecofi_list for ecofo in ecofi_item)
+                    date_to = max(ecofo.date_to for ecofi_item in ecofi_list for ecofo in ecofi_item)
+
+                    values.update({
+                        'subject': (
+                            f"Financial Summary Report From {date_from.strftime('%d-%m-%Y')} "
+                            f"To {date_to.strftime('%d-%m-%Y')}"
+                        ),
+                        'email_from': self.env.user.email,
+                        'email_to': email_to,
+                        'attachment_ids': [(6, 0, attachment.ids)],
+                        'body_html': template.body_html,
+                    })
+                else:
+                    _logger.error("eco-fi list is empty. Cannot generate email subject.")
 
                 mail = self.env['mail.mail'].sudo().create(values)
                 if mail:
