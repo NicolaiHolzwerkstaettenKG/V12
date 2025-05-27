d0e3645c21fc06f693378c584ba7651b45980ec1
EcoGitLab <gitlab@ecoservice.de>
2025-05-27 13:02:05 +0000

    Update ecoservice_german_documents_stock

--- eco/ecoservice_german_documents_stock/__manifest__.py
+++ eco/ecoservice_german_documents_stock/__manifest__.py
@@ -5,7 +5,7 @@
     'name': 'German Documents (Stock)',
     'summary': 'Designed German Documents for Odoo.',
     'category': 'Base',
-    'version': '16.0.1.2.7',
+    'version': '16.0.1.3.0',
     'license': 'OPL-1',
     'application': False,
     'installable': True,
